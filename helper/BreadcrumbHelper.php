3aa40d3a69621a32722db261af507b8146f1a199
piotrooo <piotroo89@gmail.com>
2013-09-23 20:44:59 +0200

    Add breadcrumb helper

---
+++ helper/BreadcrumbHelper.php
@@ -0,0 +1,21 @@
+<?php
+use OuzoBreadcrumb\Breadcrumb;
+
+class BreadcrumbHelper
+{
+    public static function showBreadcrumbs()
+    {
+        $breadcrumbs = '<ol class="breadcrumb">';
+        $breadcrumbsMap = Breadcrumb::getBreadcrumbs();
+        foreach ($breadcrumbsMap as $breadcrumb) {
+            if (end($breadcrumbsMap) === $breadcrumb) {
+                $attribute = 'class="active"';
+            } else {
+                $attribute = 'href="' . $breadcrumb->getPath() . '"';
+            }
+            $breadcrumbs .= '<li><a ' . $attribute . '>' . $breadcrumb->getName() . '</a></li>';
+        }
+        $breadcrumbs .= '</ol>';
+        return $breadcrumbs;
+    }
+}
